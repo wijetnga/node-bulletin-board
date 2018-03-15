c3eb81a4bbab95aaafd46edb081825b694ab012f
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2018-03-15 18:26:59 +0530

    Create file args

--- test-and-build-docker-image.sh
+++ test-and-build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t bulletin-board:${GO_PIPELINE_LABEL} . Dockerfile.application
+docker build -t bulletin-board:${GO_PIPELINE_LABEL} . -f Dockerfile.application
 docker run bulletin-board:${GO_PIPELINE_LABEL} npm test
